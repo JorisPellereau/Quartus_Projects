215c7e119fc05ad965274c003fe85f3ed608dca3
JorisP <jor.pell@hotmail.fr>
2023-10-15 12:12:54 +0200

    up quartus project with zipcpu

--- PR_115_board/ZIPCPU/ZIPCPU_AXI4_LITE/ip_jtag/altera_vjtag/simulation/mentor/msim_setup.tcl
+++ PR_115_board/ZIPCPU/ZIPCPU_AXI4_LITE/ip_jtag/altera_vjtag/simulation/mentor/msim_setup.tcl
@@ -17,7 +17,7 @@
 # ----------------------------------------
 # This script provides commands to simulate the following IP detected in
 # your Quartus project:
-#     altera_vjtag_24ir
+#     altera_vjtag
 # 
 # Altera recommends that you source this Quartus-generated IP simulation
 # script from your own customized top-level script, and avoid editing this
@@ -83,7 +83,7 @@
 # 
 # IP SIMULATION SCRIPT
 # ----------------------------------------
-# If altera_vjtag_24ir is one of several IP cores in your
+# If altera_vjtag is one of several IP cores in your
 # Quartus project, you can generate a simulation script
 # suitable for inclusion in your top-level simulation
 # script by running the following command line:
@@ -94,7 +94,7 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 20.1 720 linux 2023.09.06.11:40:46
+# ACDS 20.1 720 linux 2023.09.21.16:24:26
 
 # ----------------------------------------
 # Initialize variables
@@ -105,7 +105,7 @@ if ![info exists SYSTEM_INSTANCE_NAME] {
 }
 
 if ![info exists TOP_LEVEL_NAME] { 
-  set TOP_LEVEL_NAME "altera_vjtag_24ir"
+  set TOP_LEVEL_NAME "altera_vjtag"
 }
 
 if ![info exists QSYS_SIMDIR] { 
@@ -214,7 +214,7 @@ alias dev_com {
 alias com {
   echo "\[exec\] com"
   eval  vlog $USER_DEFINED_VERILOG_COMPILE_OPTIONS $USER_DEFINED_COMPILE_OPTIONS "$QSYS_SIMDIR/submodules/sld_virtual_jtag.v" -work virtual_jtag_0
-  eval  vcom $USER_DEFINED_VHDL_COMPILE_OPTIONS $USER_DEFINED_COMPILE_OPTIONS    "$QSYS_SIMDIR/altera_vjtag_24ir.vhd"                             
+  eval  vcom $USER_DEFINED_VHDL_COMPILE_OPTIONS $USER_DEFINED_COMPILE_OPTIONS    "$QSYS_SIMDIR/altera_vjtag.vhd"                                  
 }
 
 # ----------------------------------------
---
+++ PR_115_board/ZIPCPU/ZIPCPU_AXI4_LITE/ip_jtag/altera_vjtag_hw.tcl
@@ -0,0 +1,38 @@
+# _hw.tcl file for altera_vjtag
+package require -exact qsys 14.0
+
+# module properties
+set_module_property NAME {altera_vjtag_export}
+set_module_property DISPLAY_NAME {altera_vjtag_export_display}
+
+# default module properties
+set_module_property VERSION {1.0}
+set_module_property GROUP {default group}
+set_module_property DESCRIPTION {default description}
+set_module_property AUTHOR {author}
+
+set_module_property COMPOSITION_CALLBACK compose
+set_module_property opaque_address_map false
+
+proc compose { } {
+    # Instances and instance parameters
+    # (disabled instances are intentionally culled)
+    add_instance virtual_jtag_0 altera_virtual_jtag 20.1
+    set_instance_parameter_value virtual_jtag_0 {CREATE_PRIMITIVE_JTAG_STATE_SIGNAL_PORTS} {0}
+    set_instance_parameter_value virtual_jtag_0 {gui_use_auto_index} {0}
+    set_instance_parameter_value virtual_jtag_0 {sld_instance_index} {0}
+    set_instance_parameter_value virtual_jtag_0 {sld_ir_width} {8}
+
+    # connections and connection parameters
+    # exported interfaces
+    add_interface jtag conduit end
+    set_interface_property jtag EXPORT_OF virtual_jtag_0.jtag
+    add_interface tck clock source
+    set_interface_property tck EXPORT_OF virtual_jtag_0.tck
+
+    # interconnect requirements
+    set_interconnect_requirement {$system} {qsys_mm.clockCrossingAdapter} {HANDSHAKE}
+    set_interconnect_requirement {$system} {qsys_mm.enableEccProtection} {FALSE}
+    set_interconnect_requirement {$system} {qsys_mm.insertDefaultSlave} {FALSE}
+    set_interconnect_requirement {$system} {qsys_mm.maxAdditionalLatency} {1}
+}
