96f1872a315c7cc3fd00adbaad91309ce302a142
JorisP <jor.pell@hotmail.fr>
2024-01-16 12:39:35 +0100

    update project and rm useless files

---
+++ PR_115_board/TESTS_SYNTHESIS/Exercices_p40/scripts/design_file_list.mk
@@ -0,0 +1,11 @@
+# This is the design file list of the DESIGN
+# The files are declare in the order : Top of the file == 1st files to compile - End of the file == Last file to compile
+
+# == DESIGN FILE LIST ==
+
+
+# TOP
+DESIGN_FILE_LIST+=/home/linux-jp/Documents/GitHub/VHDL_code/PR_115/sources/lib_exercices_p40/case_sel_p40.vhd
+DESIGN_FILE_LIST+=/home/linux-jp/Documents/GitHub/VHDL_code/PR_115/sources/lib_exercices_p40/exercices_p40_top.vhd
+
+
